9076c7d33782836d69edd3831395347ac02cf41e
valera-pzc <starcoship09@gmail.com>
2025-01-30 14:43:09 +0000

    restart

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetController;
+
+
+Route::get('/', function () {
+    return 'Hello, Laravel!';
+});
+
+
+Route::get('/greet', [GreetController::class, 'showGreeting']);
